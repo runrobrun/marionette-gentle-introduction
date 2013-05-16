5f03e5684135edf7bbbeb250a1b693f2600a0a37
David Sulc <davidsulc@gmail.com>
2013-05-16 22:51:43 +0200

    toggle highlighting on clicked row

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,15 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
     List.ContactItemView = Backbone.Marionette.ItemView.extend({
       tagName: "tr",
-      template: "#contact-list-item"
+      template: "#contact-list-item",
+      
+      events: {
+        "click": "highlightName"
+      },
+    
+      highlightName: function(e){
+        this.$el.toggleClass('warning');
+      }
     });
     
     List.ContactsView = Backbone.Marionette.CompositeView.extend({
