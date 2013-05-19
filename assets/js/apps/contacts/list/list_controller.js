83d10a98d18d92288ad94821bc0176cd533d151f
David Sulc <davidsulc@gmail.com>
2013-05-19 11:00:04 +0200

    fix indentation

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -1,21 +1,21 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
-    List.Controller = {
-      listContacts: function(){
-        var contacts = ContactManager.request("contact:entities");
-        
-        var contacts_list_view = new List.ContactsView({
-          collection: contacts
-        });
+  List.Controller = {
+    listContacts: function(){
+      var contacts = ContactManager.request("contact:entities");
+      
+      var contacts_list_view = new List.ContactsView({
+        collection: contacts
+      });
 
-        contacts_list_view.on("itemview:contact:show", function(childView, model){
-          ContactManager.ContactsApp.Show.Controller.showContact(model);
-        });
+      contacts_list_view.on("itemview:contact:show", function(childView, model){
+        ContactManager.ContactsApp.Show.Controller.showContact(model);
+      });
 
-        contacts_list_view.on("itemview:contact:delete", function(childView, model){
-          contacts.remove(model);
-        });
-        
-        ContactManager.mainRegion.show(contacts_list_view);
-      }
+      contacts_list_view.on("itemview:contact:delete", function(childView, model){
+        contacts.remove(model);
+      });
+      
+      ContactManager.mainRegion.show(contacts_list_view);
     }
+  }
 });
--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,42 +1,42 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
-    List.ContactItemView = Backbone.Marionette.ItemView.extend({
-      tagName: "tr",
-      template: "#contact-list-item",
-      
-      events: {
-        "click": "highlightName",
-        "click td a.js-show": "showClicked",
-        "click td a.js-delete": "deleteClicked"
-      },
-
-      highlightName: function(e){
-        this.$el.toggleClass('warning');
-      },
-
-      showClicked: function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        this.trigger("contact:show", this.model);
-      },
-
-      deleteClicked: function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        this.trigger("contact:delete", this.model);
-      },
-
-      remove: function(){
-        this.$el.fadeOut(function(){
-          $(this).remove();
-        });
-      }
-    });
+  List.ContactItemView = Backbone.Marionette.ItemView.extend({
+    tagName: "tr",
+    template: "#contact-list-item",
     
-    List.ContactsView = Backbone.Marionette.CompositeView.extend({
-      tagName: "table",
-      className: "table table-hover",
-      template: "#contact-list",
-      itemView: List.ContactItemView,
-      itemViewContainer: "tbody"
-    });
+    events: {
+      "click": "highlightName",
+      "click td a.js-show": "showClicked",
+      "click td a.js-delete": "deleteClicked"
+    },
+
+    highlightName: function(e){
+      this.$el.toggleClass('warning');
+    },
+
+    showClicked: function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      this.trigger("contact:show", this.model);
+    },
+
+    deleteClicked: function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      this.trigger("contact:delete", this.model);
+    },
+
+    remove: function(){
+      this.$el.fadeOut(function(){
+        $(this).remove();
+      });
+    }
+  });
+  
+  List.ContactsView = Backbone.Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "table table-hover",
+    template: "#contact-list",
+    itemView: List.ContactItemView,
+    itemViewContainer: "tbody"
+  });
 });
