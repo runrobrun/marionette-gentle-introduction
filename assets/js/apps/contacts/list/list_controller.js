1ba9104b1fddd5475d2985c5944a8441d85fb7f2
David Sulc <davidsulc@gmail.com>
2013-05-24 23:04:45 +0200

    adapt application to deal with persisted data

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -12,7 +12,7 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
       });
 
       contacts_list_view.on("itemview:contact:delete", function(childView, model){
-        contacts.remove(model);
+        model.destroy();
       });
       
       ContactManager.mainRegion.show(contacts_list_view);
