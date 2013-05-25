2c1493985e93e09621b3841530eed2f06470c220
David Sulc <davidsulc@gmail.com>
2013-05-25 17:00:02 +0200

    add loading view to sub-module

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,6 +1,9 @@
 ContactManager.module('ContactsApp.Show', function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
+      var loading_view = new ContactManager.Common.Views.Loading();
+      ContactManager.mainRegion.show(loading_view);
+
       var fetching_contact = ContactManager.request("contact:entity", id);
       $.when(fetching_contact).done(function(contact){
         var contact_view;
