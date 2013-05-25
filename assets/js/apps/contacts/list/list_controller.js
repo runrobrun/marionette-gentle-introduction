344c45f776a24d13462e21068ec3402e86c50f80
David Sulc <davidsulc@gmail.com>
2013-05-25 15:33:02 +0200

    handling data latency

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -1,21 +1,23 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
   List.Controller = {
     listContacts: function(){
-      var contacts = ContactManager.request("contact:entities");
+      var fetching_contacts = ContactManager.request("contact:entities");
       
-      var contacts_list_view = new List.ContactsView({
-        collection: contacts
-      });
+      $.when(fetching_contacts).done(function(contacts){
+        var contacts_list_view = new List.ContactsView({
+          collection: contacts
+        });
 
-      contacts_list_view.on("itemview:contact:show", function(childView, model){
-        ContactManager.trigger("contact:show", model.get('id'));
-      });
+        contacts_list_view.on("itemview:contact:show", function(childView, model){
+          ContactManager.trigger("contact:show", model.get('id'));
+        });
 
-      contacts_list_view.on("itemview:contact:delete", function(childView, model){
-        model.destroy();
+        contacts_list_view.on("itemview:contact:delete", function(childView, model){
+          model.destroy();
+        });
+        
+        ContactManager.mainRegion.show(contacts_list_view);
       });
-      
-      ContactManager.mainRegion.show(contacts_list_view);
     }
   }
 });
--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -1,18 +1,20 @@
 ContactManager.module('ContactsApp.Show', function(Show, ContactManager, Backbone, Marionette, $, _){
   Show.Controller = {
     showContact: function(id){
-      var contact = ContactManager.request("contact:entity", id);
-      var contact_view;
-      if(contact !== undefined){
-        contact_view = new Show.Contact({
-          model: contact
-        });
-      }
-      else{
-        contact_view = new Show.MissingContact();
-      }
-      
-      ContactManager.mainRegion.show(contact_view);
+      var fetching_contact = ContactManager.request("contact:entity", id);
+      $.when(fetching_contact).done(function(contact){
+        var contact_view;
+        if(contact !== undefined){
+          contact_view = new Show.Contact({
+            model: contact
+          });
+        }
+        else{
+          contact_view = new Show.MissingContact();
+        }
+
+        ContactManager.mainRegion.show(contact_view);
+      });
     }
   }
 });
